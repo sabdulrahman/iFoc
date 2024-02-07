58d405efe2b8c865fd895b4e6659acf759ab6ad0
sabdulrahman <sabdulrahman095@gmail.com>
2024-02-07 01:34:32 -0600

    Updated

---
+++ views/consts.js
@@ -0,0 +1,24 @@
+monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+dayNames = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
